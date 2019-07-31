a8c772bed07e2b37bcd379e97be18c178c284e9b
ikramaelahi1 <ikramaelahi1@gmail.com>
2019-07-31 05:29:05 +0500

    computerly

---
+++ app/src/main/java/com/example/computerly/splash_screen.java
@@ -0,0 +1,32 @@
+package com.example.computerly;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class splash_screen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(splash_screen.this,MainActivity.class);
+                splash_screen.this.startActivity(mainIntent);
+                splash_screen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+}
